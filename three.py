b268be660757d9390e20ff4704f5cb2c41582576
kidistendashaw <kidistendashaw16@gmail.com>
2025-07-29 14:17:05 +0300

    lsd

--- three.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter your name:")
-if name == "kidist":
-    print("Hello kidist!")
-elif name == "kidi":
-    print("Hello kidist!")
-elif name == ("kidiste"):
-    print("Hello kidist!")
-else:
-    print("What is your name ? you are not kidist!")
--- two.py
+++
@@ -1,9 +0,0 @@
-loc = input("Enter your name :")
-if loc == "kidist":
-    print("may be")
-elif loc == "abu":
-    print("may be")
-elif loc == "ba":
-    print("yes")
-else:
-    print("No one can play this game")
